9850961a3188a45cbfcec0d2990f8c6ca66e2344
Tureczek <55349594+Tureczek@users.noreply.github.com>
2021-12-11 11:44:57 +0100

    Question search with path variable added to controller

--- SI-Exam-QACategorization/src/main/java/siexamqacategorization/Controller/AnswerController.java
+++ SI-Exam-QACategorization/src/main/java/siexamqacategorization/Controller/AnswerController.java
@@ -3,13 +3,16 @@ package siexamqacategorization.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import siexamqacategorization.repository.AnswerRepository;
 import siexamqacategorization.model.Answers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("answers")
 public class AnswerController {
 
     @Autowired
@@ -21,9 +24,27 @@ public class AnswerController {
         return answerRepository.findAll();
     }
 
+    @GetMapping("/a/{question}")
+    public List<Answers> searchQuestions(@PathVariable String question) {
+        // List of all questions
+        List<Answers> myList = answerRepository.findAll();
+        // List for similar questions to the one which has been asked
+        List<Answers> arr = new ArrayList<Answers>();
+
+
+        for (int i = 0; i < myList.size(); i++){
+           if(myList.get(i).getQuestion().contains(question)){
+               System.out.println(myList.get(i));
+               arr.add(myList.get(i));
+           }
+        }
+        return arr;
+    }
+
     @GetMapping("/{id}")
     public String getAnswerById(@PathVariable long id) {
         Answers answers = answerRepository.getById(id);
+
         System.out.println(answers);
         return "NO!!!";
     }
--- SI-Exam-QACategorization/src/main/java/siexamqacategorization/repository/AnswerRepository.java
+++ SI-Exam-QACategorization/src/main/java/siexamqacategorization/repository/AnswerRepository.java
@@ -1,8 +1,14 @@
 package siexamqacategorization.repository;
-
+//https://www.baeldung.com/rest-api-search-language-spring-data-specifications
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import siexamqacategorization.model.Answers;
 
+import java.util.List;
+
+@Repository
 public interface AnswerRepository extends JpaRepository<Answers, Long> {
+
     // Notice, there are no methods here, but we still can use all those, which we inherit from JpaRepository
 }
